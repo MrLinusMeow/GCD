a813c3dd082a3779507a2874ea147de60eac6bf8
MrLinusMeow <66890270+MrLinusMeow@users.noreply.github.com>
2025-02-26 16:19:41 +0100

    assert innit

--- main.c
+++ main.c
@@ -29,6 +29,7 @@ main(int argc, char* argv[]){
      argv[1] = "19";
      break;
     }
+    assert(0);
   }
   fprintf(\
     stderr,\
@@ -39,60 +40,60 @@ main(int argc, char* argv[]){
   );
   return -1;
  }
- for(int i = argc-1; i > 0; --i){
-  int c = 0;
-  assert(argv[i][c] != 0);
+ for(int x = argc-1; x > 0; --x){
+  int y = 0;
+  assert(argv[x][y] != 0);
   do{
-   if(isdigit((int)argv[i][c])) continue;
+   if(isdigit((int)argv[x][y])) continue;
    else{
-    fprintf(stderr, "Argument \'%s\' number %d is not digit.\n", argv[i], i);
-    argc = 0;
+    fprintf(stderr, "Argument \'%s\' number %d is not digit.\n", argv[x], x);
+    argc = -1;
     continue;
    }
-  }while(argv[i][++c] != 0);
+   assert(0);
+  }while(argv[x][++y] != 0);
+  continue;
  }
- if(argc == 0) return -1;
- else{
-  assert(argc >= 3);
-  int index = argc;
-  int digits[index];
-  digits[0] = 0;
-  for(int n = 1; n < index; ++n){
-   assert(isdigit(*argv[n]));
-   digits[n] = atoi(argv[n]);
+ if(argc == -1) return -1;
+
+ assert(argc >= 3);
+ int index = argc;
+ int digits[index];
+ digits[0] = 0;
+ for(int n = 1; n < index; ++n){
+  assert(isdigit(*argv[n]));
+  digits[n] = atoi(argv[n]);
+  continue;
+ }
+ while(--index > 1){
+  assert(index != argc);
+  assert(digits[index] != digits[index-1]);
+  if(digits[index] > digits[index-1]){
+   int t = digits[index];
+   digits[index] = digits[index-1];
+   digits[index-1] = t;
+   if(index < argc-1) index += 2;
    continue;
-  }
-  while(--index > 1){
-   assert(index != argc);
-   assert(digits[index] != digits[index-1]);
-   if(digits[index] > digits[index-1]){
-    int t = digits[index];
-    digits[index] = digits[index-1];
-    digits[index-1] = t;
-    if(index < argc-1) index += 2;
-    continue;
+  }else continue;
+  assert(0);
+ }
+ assert(digits[0] == 0);
+ int pointer = 0;
+ int n = 0;
+ int m = 0;
+ while(++pointer < (argc-1)){
+  assert(pointer != 0);
+  n = digits[pointer];
+  m = digits[pointer+1];
+  assert(n > m);
+  for(int k = m; k > 0; --k){
+   if( ((m%k) == 0) && ((n%k) == 0) ){
+    digits[0] = k;
+    break;
    }else continue;
   }
-  assert(digits[0] == 0);
-  if(digits[1] == digits[2] && (argc == 3)){
-   digits[0] = 1;
-  }
-  int pointer = 0;
-  int n = 0;
-  int m = 0;
-  while(++pointer < (argc-1)){
-   assert(pointer != 0);
-   n = digits[pointer];
-   m = digits[pointer+1];
-   assert(n > m);
-   for(int k = m; k > 0; --k){
-    if( ((m%k) == 0) && ((n%k) == 0) ){
-     digits[0] = k;
-     break;
-    }else continue;
-   }
-  }
-  fprintf(stdout, "GCD: %d\n", digits[0]);
-  return 0;
+  continue;
  }
+ fprintf(stdout, "GCD: %d\n", digits[0]);
+ return 0;
 }
